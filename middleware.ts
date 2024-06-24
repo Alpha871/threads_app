c7f2cb36ce3aa8062ea274233e0afcc1ae1242fe
alpha871 <bahalphaabdoulaye40@gmail.com>
2024-06-24 20:28:37 +0300

    webhook

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,15 @@
-import {
-  auth,
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // const isPublicRoutes = createRouteMatcher(["/", "api/webhooks/clerk"]);
 // const isIgnoredRoutes = createRouteMatcher(["/", "api/webhooks/clerk"]);
 
 // export default clerkMiddleware((auth, request) => {
-//   if (!isPublicRoutes(request)) {
+//   const url = request.nextUrl.pathname;
+
+//   // Log the request URL for debugging
+//   console.log(`Request URL: ${url}`);
+
+//   if (!isPublicRoutes(request) || !isIgnoredRoutes(request)) {
 //     auth().protect();
 //   }
 // });
